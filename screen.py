cf51db092de79578bb580565fe4be6bf4e26b52c
Nicodemes <sasha@paticon.com>
2014-11-29 16:54:01 +0200

    add actions to screen

--- screen.py
+++ screen.py
@@ -1,3 +1,5 @@
+from common import *
+from cpu_ref import *
 class Screen(LinkedComponent):
 	def __init__(self,**f):
 		self.frames=f['frames']
@@ -7,23 +9,38 @@ class Screen(LinkedComponent):
 		self.tmpcursor=f['tmpcursor']
 		self.iChar=f['iChar']
 		self.charpivot=f['charpivot']
-		self.snapStart=f['snapStart']
+		self.snapStart=f['snapstart']
 		
 	def clearAction(self):
 		return "/fill {} {} {}".format(self.tmpcursor.dest,self.iChar.getEndOfOrigin(),Pear.resetBlock)
 	def char(self,i):
-		a=Point(self.snapStart[0][0],self.snapStart[0][1]+i,self.snapStart[0][2])
-		b=Point(self.snapStart[1][0],self.snapStart[1][1]+i,self.snapStart[1][2])
+		a=Point(self.snapStart[0][0],self.snapStart[0][1]-i,self.snapStart[0][2])
+		b=Point(self.snapStart[1][0],self.snapStart[1][1]-i,self.snapStart[1][2])
 		return self.cursor.execute("/clone {} {} ~ ~ ~".format(a,b))
+	def action(self,i):
+		CHARSIZE=(6,8)
+		if i <=4:
+			return self.cursor.moveX(CHARSIZE[0]*(2**i))
+		else:
+			return self.cursor.moveZ(CHARSIZE[0]*(2**(i-5)))
+	
+	def charselect(self,i):
+		if i<=3:
+			return self.charpivot.moveZ(2**i)
+		else:
+			return self.charpivot.moveX(-(2**(i-4)))
 	def __getitem__(self,i):
 		return self.frames[i]
+	def printc(self):
+		return self.charpivot.signal()
 screen=Screen(
-	snapstart=((-199,0,-162)(-194,0,-155)),
+	snapstart=((-199,104,-162),(-194,104,-155)),
 	frames=[
-		Point("-202 12 -189"),
-		Point("-201 12 -189"),
-		Point("-198 12 -189")
+		Pear("-202 12 -189"),
+		Pear("-201 12 -189"),
+		Pear("-198 12 -189")
 	],
+	iCursor=Pear("-202 14 -192",8),
 	tmpcursor=Pear("-202 11 -192",8),
 	iChar=Pear("-202 11 -191",8),
 	charpivot=pRefrence("SCREEN_CHARPIVOT",Point("-194 12 -180")),
